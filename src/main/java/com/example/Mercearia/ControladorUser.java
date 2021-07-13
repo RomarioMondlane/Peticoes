fc7193e1f14ef605c9cba8be6cf108f046625b65
RomarioMondlane <romariomondlane88@gmail.comclear>
2021-07-13 20:58:15 +0200

    mercearia v.1

---
+++ src/main/java/com/example/Mercearia/ControladorUser.java
@@ -0,0 +1,11 @@
+package com.example.Mercearia;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ControladorUser extends JpaRepository<User, String> {
+    	
+	public User findByusername(String username);
+}
